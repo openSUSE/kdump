9cf82a2445e12579747d89a789ce1986549f185d
Bernhard Walle <bwalle@suse.de>
2008-07-27 22:45:22 +0200

    Exclude '/' from being added to fstab
    The root file system is handled differently

--- init/setup-kdump.sh
+++ init/setup-kdump.sh
@@ -100,6 +100,7 @@ while read line ; do
 
     # add the target file system
     if [ "$protocol" = "file" ] &&
+            [ "$path" != "/" ] &&
             echo "$path" | grep "$mountpoint" &> /dev/null ; then
         echo "$line" >> ${tmp_mnt}/etc/fstab
     fi
