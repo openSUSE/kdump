152862de8524be462ea38e7f45bc124269a4743b
Bernhard Walle <bwalle@suse.de>
2008-07-27 23:22:04 +0200

    Actually _use_ gawk

--- init/boot-kdump.sh
+++ init/boot-kdump.sh
@@ -17,7 +17,7 @@
 #
 #%stage: setup
 #%depends: mount network
-#%programs: /bin/makedumpfile /bin/awk /bin/awk /bin/grep /sbin/kdumptool /sbin/showmount /sbin/mount.cifs mount.nfs /bin/gdb
+#%programs: /bin/makedumpfile /bin/awk /bin/gawk /bin/grep /sbin/kdumptool /sbin/showmount /sbin/mount.cifs mount.nfs /bin/gdb
 #%if: "$use_kdump"
 #%udevmodules: cifs nls_utf8 reiserfs xfs jfs ext2 ext3
 
