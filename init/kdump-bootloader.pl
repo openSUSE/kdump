c26cf2b08dd25cc6a5c8a0bbe5a0d70671885b9a
Petr Tesarik <ptesarik@suse.cz>
2013-02-08 14:38:28 +0100

    Add kdump-bootloader.pl for updating the boot loader config
    This piece must be written in Perl, because we don't want to re-implement
    Perl-Bootloader.

---
+++ init/kdump-bootloader.pl
@@ -0,0 +1,36 @@
+#! /usr/bin/perl
+
+use Bootloader::Tools;
+
+Bootloader::Tools::InitLibrary();
+
+if ($ARGV[0] eq "--get") {
+    my $section = Bootloader::Tools::GetDefaultSection ();
+    print $section->{"append"};
+} elsif ($ARGV[0] eq "--update") {
+    my $section = Bootloader::Tools::GetDefaultSection ();
+    my $input = $section->{"append"};
+    my $result;
+    while (length($input)) {
+    	$input =~ s/^[[:space:]]+//;
+	if ($input =~ s/^("[^"]*"?|[^"[:space:]]+)+//) {
+	    my $rawparam = $&;
+	    my $param = $rawparam;
+	    $param =~ s/"//g;
+	    $param =~ s/=(.*)//;
+	    if (! ($param =~ /^(KDUMP|MAKEDUMPFILE)_/)) {
+		$result .= " " if length($result);
+		$result .= $rawparam;
+	    }
+	}
+    }
+
+    shift @ARGV;
+    $result .= " " if length($result);
+    $result .= join(" ", @ARGV);
+    $section->{"append"} = $result;
+    $section->{"__modified"} = 1;
+    Bootloader::Tools::SetGlobals();
+} else {
+    die "Need an action (--get or --update)";
+}
