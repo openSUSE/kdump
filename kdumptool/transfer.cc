openSUSE/kdump

commit 80c41c34b534158ab42254b5cc5bde78dc102493

Move mkdir() and rmdir() to FilePath
Signed-off-by: Petr Tesarik <ptesarik@suse.cz>

kdumptool/transfer.cc
@@ -131,7 +131,7 @@ FileTransfer::FileTransfer(const RootDirURLVector &urlv,
     for (it = urlv.begin(); it != urlv.end(); ++it) {
         FilePath dir = it->getRealPath();
         dir.appendPath(subdir);
-        FileUtil::mkdir(dir, true);
+        dir.mkdir(true);
     }
 
     // try to get the buffer size
@@ -627,7 +627,8 @@ SFTPTransfer::SFTPTransfer(const RootDirURLVector &urlv,
     }
 
     FilePath fp = parser.getPath();
-    mkdir(fp.appendPath(subdir), true);
+    fp.appendPath(subdir);
+    fp.mkdir(true);
 }
 
 /* -------------------------------------------------------------------------- */

kdumptool/deletedumps.cc
@@ -132,7 +132,7 @@ void DeleteDumps::delete_one(const RootDirURL &url, int oldDumps)
         if (!m_dryRun) {
             FilePath fp = dir;
             fp.appendPath(*it);
-            FileUtil::rmdir(fp, true);
+            fp.rmdir(true);
         }
     }
 }

kdumptool/fileutil.cc
@@ -59,38 +59,6 @@ string FileUtil::getcwd(void)
 }
 
 // -----------------------------------------------------------------------------
-void FileUtil::mkdir(const std::string &dir, bool recursive)
-    throw (KError)
-{
-    Debug::debug()->trace("mkdir(%s, %d)", dir.c_str(), int(recursive));
-
-    if (!recursive) {
-        Debug::debug()->dbg("::mkdir(%s)", dir.c_str());
-        int ret = ::mkdir(dir.c_str(), 0755);
-        if (ret != 0 && errno != EEXIST)
-            throw KSystemError("mkdir of " + dir + " failed.", errno);
-    } else {
-        string directory = dir;
-
-        // remove trailing '/' if there are any
-        while (directory[directory.size()-1] == '/')
-            directory = directory.substr(0, directory.size()-1);
-
-        string::size_type current_slash = 0;
-
-        while (true) {
-            current_slash = directory.find('/', current_slash+1);
-            if (current_slash == string::npos) {
-                FileUtil::mkdir(directory, false);
-                break;
-            }
-
-            mkdir(directory.substr(0, current_slash), false);
-        }
-    }
-}
-
-// -----------------------------------------------------------------------------
 void FileUtil::nfsmount(const string &host,
                         const string &dir,
                         const string &mountpoint,
@@ -207,46 +175,6 @@ StringVector FileUtil::listdir(const std::string &dir, bool onlyDirs)
     free(namelist);
 
     return v;
-}
-
-// -----------------------------------------------------------------------------
-void FileUtil::rmdir(const std::string &dir, bool recursive)
-    throw (KError)
-{
-    Debug::debug()->trace("FileUtil::rmdir(%s, %d)", dir.c_str(), recursive);
-
-    if (recursive) {
-        DIR *dirptr = opendir(dir.c_str());
-        if (!dirptr)
-            throw KSystemError("Cannot opendir(" + dir + ").", errno);
-        struct dirent *ptr;
-        try {
-            while ((ptr = readdir(dirptr)) != NULL) {
-                FilePath fn = dir;
-                if (strcmp(ptr->d_name, ".") == 0 ||
-                        strcmp(ptr->d_name, "..") == 0)
-                    continue;
-                if (ptr->d_type == DT_DIR)
-                    rmdir(fn.appendPath(ptr->d_name).c_str(), true);
-                else {
-                    Debug::debug()->trace("Calling remove(%s)", ptr->d_name);
-                    int ret = ::remove(fn.appendPath(ptr->d_name).c_str());
-                    if (ret != 0)
-                        throw KSystemError("Cannot remove " +
-                            string(ptr->d_name) + ".", errno);
-                }
-            }
-        } catch (...) {
-            closedir(dirptr);
-            throw;
-        }
-        closedir(dirptr);
-        rmdir(dir, false);
-    } else {
-        int ret = ::rmdir(dir.c_str());
-        if (ret != 0)
-            throw KSystemError("Cannot rmdir(" + dir + ").", errno);
-    }
 }
 
 // -----------------------------------------------------------------------------
@@ -436,6 +364,77 @@ FilePath FilePath::getCanonicalPath(const string &root) const
     }
 
     return ret;
+}
+
+// -----------------------------------------------------------------------------
+void FilePath::mkdir(bool recursive)
+    throw (KError)
+{
+    Debug::debug()->trace("mkdir(%s, %d)", c_str(), int(recursive));
+
+    if (!recursive) {
+        Debug::debug()->dbg("::mkdir(%s)", c_str());
+        int ret = ::mkdir(c_str(), 0755);
+        if (ret != 0 && errno != EEXIST)
+            throw KSystemError("mkdir of " + *this + " failed.", errno);
+    } else {
+        FilePath directory = *this;
+
+        // remove trailing '/' if there are any
+        while (directory[directory.size()-1] == '/')
+            directory = directory.substr(0, directory.size()-1);
+
+        size_type current_slash = 0;
+
+        while (true) {
+            current_slash = directory.find('/', current_slash+1);
+            if (current_slash == npos) {
+                directory.mkdir(false);
+                break;
+            }
+
+            FilePath fp = directory.substr(0, current_slash);
+            fp.mkdir(false);
+        }
+    }
+}
+
+// -----------------------------------------------------------------------------
+void FilePath::rmdir(bool recursive)
+    throw (KError)
+{
+    Debug::debug()->trace("FileUtil::rmdir(%s, %d)", c_str(), recursive);
+
+    if (recursive) {
+        DIR *dirptr = opendir(c_str());
+        if (!dirptr)
+            throw KSystemError("Cannot opendir(" + *this + ").", errno);
+        struct dirent *ptr;
+        try {
+            while ((ptr = readdir(dirptr)) != NULL) {
+                FilePath fn = *this;
+                if (strcmp(ptr->d_name, ".") == 0 ||
+                        strcmp(ptr->d_name, "..") == 0)
+                    continue;
+                if (ptr->d_type == DT_DIR)
+                    fn.appendPath(ptr->d_name).rmdir(true);
+                else {
+                    Debug::debug()->trace("Calling remove(%s)", ptr->d_name);
+                    int ret = ::remove(fn.appendPath(ptr->d_name).c_str());
+                    if (ret != 0)
+                        throw KSystemError("Cannot remove " +
+                            string(ptr->d_name) + ".", errno);
+                }
+            }
+        } catch (...) {
+            closedir(dirptr);
+            throw;
+        }
+        closedir(dirptr);
+    }
+    int ret = ::rmdir(c_str());
+    if (ret != 0)
+        throw KSystemError("Cannot rmdir(" + *this + ").", errno);
 }
 
 //}}}

kdumptool/fileutil.h
@@ -40,18 +40,6 @@ class FileUtil {
         throw (KError);
 
         /**
-         * Creates a new directory.
-         *
-         * @param[in] dir the name of the directory that should be created
-         * @param[in] recursive @b true if the behaviour of <tt>mkdir -p</tt>
-         *            should be copied, @c false otherwise.
-         *
-         * @throw KError on any error
-         */
-        static void mkdir(const std::string &dir, bool recursive)
-        throw (KError);
-
-        /**
          * Mounts a file system to a given mountpoint.
          *
          * @param[in] device the device or a CIFS share or a NFS target
@@ -110,17 +98,6 @@ class FileUtil {
           * @exception KError if something went wrong
           */
          static StringVector listdir(const std::string &dir, bool onlyDirs)
-         throw (KError);
-
-         /**
-          * Delete the specified directory.
-          *
-          * @param[in] dir the name of the directory
-          * @param[in] recursive @c true if all contents of non-empty
-          *            directories should be deleted, @c false otherwise
-          * @exception KError if something went wrong
-          */
-         static void rmdir(const std::string &dir, bool recursive)
          throw (KError);
 
          /**
@@ -248,6 +225,27 @@ class FilePath : public KString {
          * @throw KError when chroot or realpath() fail
          */
         FilePath getCanonicalPath(const std::string &root = m_slash) const
+        throw (KError);
+
+        /**
+         * Creates a new directory in the specified path.
+         *
+         * @param[in] recursive @b true if the behaviour of <tt>mkdir -p</tt>
+         *            should be copied, @c false otherwise.
+         *
+         * @throw KError on any error
+         */
+        void mkdir(bool recursive)
+        throw (KError);
+
+        /**
+         * Delete the specified directory.
+         *
+         * @param[in] recursive @c true if all contents of non-empty
+         *            directories should be deleted, @c false otherwise
+         * @exception KError if something went wrong
+         */
+        void rmdir(bool recursive)
         throw (KError);
 
 };

kdumptool/savedump.cc
@@ -637,7 +637,7 @@ void SaveDump::check_one(const RootDirURL &parser,
             bytes_to_megabytes(freeSize), targetDiskSize);
 
     if (bytes_to_megabytes(freeSize) < targetDiskSize) {
-        FileUtil::rmdir(path, true);
+        path.rmdir(true);
         throw KError("Dump too large. Aborting. Check KDUMP_FREE_DISK_SIZE.");
     }
 }

commit 51e18bc86bede439a5d918d1e61459a88392c7d9

Create SSH directories on remote host, not local
Revert a silly bug introduced in
commit 80c41c34b534158ab42254b5cc5bde78dc102493

SFTPTransfer::mkdir() is very different from FilePath::mkdir().

Signed-off-by: Petr Tesarik <ptesarik@suse.cz>

kdumptool/transfer.cc
@@ -626,8 +626,7 @@ SFTPTransfer::SFTPTransfer(const RootDirURLVector &urlv,
     }
 
     FilePath fp = parser.getPath();
-    fp.appendPath(subdir);
-    fp.mkdir(true);
+    mkdir(fp.appendPath(subdir), true);
 }
 
 /* -------------------------------------------------------------------------- */