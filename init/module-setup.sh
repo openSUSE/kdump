055531f09ceb18f9880c9220d253a09f4afec95c
Petr Tesarik <ptesarik@suse.cz>
2014-05-30 13:24:24 +0200

    Store current configuration if KDUMP_NETCONFIG is static
    This improves compatibility with SLES11 kdump. Since dracut misses
    some of the required features, the network configuration is resolved
    prior to running dracut.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.cz>

--- init/module-setup.sh
+++ init/module-setup.sh
@@ -22,4 +22,5 @@ install() {
     inst_hook pre-pivot 90 /lib/kdump/save_dump.sh
     inst_multiple makedumpfile makedumpfile-R.pl kdumptool \
 	$KDUMP_REQUIRED_PROGRAMS
+    inst_simple /etc/resolv.conf
 }
