9940475e501273ddac4a13fa2f4284b4121415de
Petr Tesarik <ptesarik@suse.cz>
2014-09-18 15:40:37 +0200

    Dracut-systemd support
    Adjust the dracut module to work with the systemd module.
    
    A new service is added to the initrd. I could not use a pre-pivot
    hook, because dracut-pre-pivot.service does not send the output
    to the terminal, so dump progress was not visible.
    
    It is also necessary to avoid mounting the root filesystem. All
    filesystems needed by kdump are mounted under /kdump and set up
    with the --mount dracut option.
    
    The system root mount can be generated in two ways:
    
    a. by initrd-fstab-generator (if "root=" is passed on the kernel
       command line)
    
    b. by dracut-rootfs-generator (if the root option is stored in
       /etc/cmdline.d/*)
    
    The root= parameter is already removed from the command line, but
    it is always saved to /etc/cmdline.d/95root-dev.conf if dracut is
    called with --hostonly-cmdline. This is the default in SLES (cf.
    /etc/dracut.conf.d/01-dist.conf) and it is required to get correct
    configuration for many setups.
    
    So, the only way to avoid mounting /sysroot is to remove the
    appropriate systemd generator from the initrd.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.cz>

--- init/module-setup.sh
+++ init/module-setup.sh
@@ -18,8 +18,18 @@ install() {
 
     kdump_setup_files "$initdir" "${!host_fs_types[*]}"
 
-    inst_hook mount 30 "$moddir/mount-kdump.sh"
-    inst_hook pre-pivot 90 /lib/kdump/save_dump.sh
+    if dracut_module_included "systemd" ; then
+	rm -f "${initdir}/$systemdutildir"/system-generators/dracut-rootfs-generator
+	inst_simple /lib/kdump/save_dump.sh
+	inst_simple "$moddir/kdump-save.service" \
+	    "$systemdsystemunitdir"/kdump-save.service
+	ln_r "$systemdsystemunitdir"/kdump-save.service \
+	    "$systemdsystemunitdir"/initrd.target.wants/kdump-save.service
+    else
+	inst_hook mount 30 "$moddir/mount-kdump.sh"
+	inst_hook pre-pivot 90 /lib/kdump/save_dump.sh
+    fi
+
     inst_multiple makedumpfile makedumpfile-R.pl kdumptool \
 	$KDUMP_REQUIRED_PROGRAMS
     inst_simple /etc/resolv.conf
