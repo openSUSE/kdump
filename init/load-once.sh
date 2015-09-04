6859c6c7e8abd0f141ab4709d68d5e50800de80a
Petr Tesarik <ptesarik@suse.com>
2015-09-04 10:16:25 +0200

    Reload kdump only once if multiple udev events happen in parallel
    The kexec(2) system call is not re-entrant, so kdump reload may fail
    if another reload is already in progress. Solve this by serializing
    the requests using a lock file under /var/run.
    
    Note that it is sufficient to re-run kexec(1) once after all memory
    configuration changes are finished. To avoid unnecessary runs, the
    following logic is implemented: If a reload is already in progress,
    all subsequent reloads exit immediately, letting the running instance
    redo the job. A jobs file is used to tell the running instance that
    there are more changes after the first one, so it must reload the
    kdump kernel one more time.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

---
+++ init/load-once.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+# This script ensures that kdump is (re-)loaded once.
+# It prevents multiple invocation of load.sh even if multiple instances
+# of the script are run in parallel. The first instance repeats the action
+# as long as there is any pending work.
+
+LOCKFILE=/var/run/kdump.lock
+JOBSFILE=/var/run/kdump.jobs
+export JOBSFILE
+
+# Add a pending job
+echo $$ >> "$JOBSFILE"
+
+# Make sure lock-file exists
+> "$LOCKFILE"
+
+flock -n "$LOCKFILE" -c '
+    while [ -s "$JOBSFILE" ]
+    do
+        truncate -s 0 "$JOBSFILE" || exit 1
+        /lib/kdump/load.sh
+    done'
