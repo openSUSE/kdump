118baa0802df211e06439071412ff2840954b0cd
Petr Tesarik <ptesarik@suse.com>
2015-10-05 14:42:08 +0200

    Limit number of udev workers to 8
    The calibration code assumes 8 workers, but systemd-udevd currently
    calculates the max number of workers as 8 + 64 * cpu_count.
    
    This number might need increasing if there is enough RAM and kdump
    boot is too slow.
    
    References: bsc#944606, bsc#947825
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- init/load.sh
+++ init/load.sh
@@ -66,12 +66,12 @@ function build_kdump_commandline()
         local nr_cpus=$(cpus_param "$kdump_kernel")
         commandline=$(
             remove_from_commandline \
-                'root|resume|crashkernel|splash|mem|BOOT_IMAGE|showopts|zfcp\.allow_lun_scan|hugepages|acpi_no_memhotplug|cgroup_disable|unknown_nmi_panic' \
+                'root|resume|crashkernel|splash|mem|BOOT_IMAGE|showopts|zfcp\.allow_lun_scan|hugepages|acpi_no_memhotplug|cgroup_disable|unknown_nmi_panic|rd\.udev\.children-max' \
                 < /proc/cmdline)
         # Use deadline for saving the memory footprint
         commandline="$commandline elevator=deadline sysrq=yes reset_devices acpi_no_memhotplug cgroup_disable=memory"
         commandline="$commandline irqpoll ${nr_cpus}=${KDUMP_CPUS:-1}"
-        commandline="$commandline root=kdump"
+        commandline="$commandline root=kdump rd.udev.children-max=8"
         case $(uname -i) in
         i?86|x86_64)
             local boot_apicid=$(
