openSUSE/kdump

commit 2ad80dd96b91b889db44dd8c955cb127840a2be2

Do not add 'rd.neednet=1' to dracut command line
According to Thomas Blume, this parameter tries to set up an IP
address on all configured interfaces. That fails e.g. for enslaved
interfaces, which do not have an IP address.

References: bsc#1177196
Signed-off-by: Petr Tesarik <ptesarik@suse.com>

init/module-setup.sh
@@ -198,7 +198,6 @@ kdump_cmdline_ip() {
 
     kdump_ifname_config "$kdump_host_if"
 
-    echo -n "rd.neednet=1"
     echo -n "$kdump_netif"
 
     case "$kdump_net_mode" in

commit fe0777beff90a2479b451f5ef9e918249277c8ff

Add 'bootdev=' to dracut command line
If this parameter is missing, dracut will parse the ip= command
line option(s) and create the corresponding initqueue jobs, but it
will not actually run them, unless they are needed to mount the
root filesystem.

References: bsc#1182309
Fixes: 2ad80dd96b91b889db44dd8c955cb127840a2be2
Signed-off-by: Petr Tesarik <ptesarik@suse.com>

init/module-setup.sh
@@ -213,6 +213,7 @@ kdump_cmdline_ip() {
 
     kdump_ifname_config "$kdump_host_if"
 
+    echo -n "bootdev=$kdump_iface"
     echo -n "$kdump_netif"
 
     case "$kdump_net_mode" in