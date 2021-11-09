2c5a679676009aec6a13db3bfb0cc8fc7223509a
Hari Bathini <hbathini@linux.ibm.com>
2021-11-09 19:44:42 +0530

    fadump: isolate fadump initrd image within the default one
    In case of fadump, the initrd image has to be built to boot into the
    production environment as well as to offload the active crash dump to
    the specified dump target (for boot after crash). As the same image
    would be used for both boot scenarios, it could not be built optimally
    while accommodating both cases.
    
    Swtich to dracut instead of mkinitrd to build the initrd image. And
    use '--include' dracut option to include the initrd image, built for
    offloading active crash dump, into the default initrd. Also, introduce
    a new out-of-tree dracut module (99zz-fadumpinit) that installs a
    customized init program while moving default /init to /init.dracut.
    This customized init program is leveraged to isolate fadump image
    within the default initrd image by kicking off default boot process
    (exec /init.dracut) for regular boot scenario and activating fadump
    initrd image, if the system is booting after crash.
    
    Signed-off-by: Hari Bathini <hbathini@linux.ibm.com>

---
+++ init/fadump-module-setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+check() {
+    return 255
+}
+
+depends() {
+    return 0
+}
+
+install() {
+    mv -f "$initdir/init" "$initdir/init.dracut"
+    inst_script "$moddir/init-fadump.sh" /init
+    chmod a+x "$initdir/init"
+
+    # Install required binaries for the init script (init-fadump.sh)
+    inst_multiple sh modprobe grep mkdir mount ls mv switch_root
+}
