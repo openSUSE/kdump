7d327fa63c6c6bc6c5f4c5e96140eedc19535499
Petr Tesarik <ptesarik@suse.com>
2021-11-22 15:23:45 +0100

    Clean up fadump-module-setup.sh
    Adapt the fadump module-setup to match changes to init-fadump in
    the recent commits:
    
    - The program is no longer a script.
    - No additional binaries are required.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- init/fadump-module-setup.sh
+++ init/fadump-module-setup.sh
@@ -10,9 +10,6 @@ depends() {
 
 install() {
     mv -f "$initdir/init" "$initdir/init.dracut"
-    inst_script "$moddir/init-fadump" /init
+    inst_binary "$moddir/init-fadump" /init
     chmod a+x "$initdir/init"
-
-    # Install required binaries for the init script (init-fadump.sh)
-    inst_multiple sh modprobe grep mkdir mount ls mv switch_root
 }
