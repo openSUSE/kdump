openSUSE/kdump

commit 7ac0bd39c364c13979ba49055045ee4042fe8bea

Convert all easy cases to the new SubProcess API
The conversion of most old API users is straightforward.

The testprocess program is a bit more complex. No exceptions are
raised with the new API, because a nullptr can be returned
instead. The tests for the correct type are a bit pointless now,
but at least it checks the API calls themselves.

This leaves ProcessFilter::IO and descendants, but I'm moving that
to a separate commit to avoid cluttering this one.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

kdumptool/sshtransfer.cc
@@ -20,6 +20,8 @@
 #include <string>
 #include <cstdlib>
 #include <cerrno>
+#include <memory>
+
 #include <stdint.h>
 #include <unistd.h>
 
@@ -36,6 +38,7 @@
 using std::string;
 using std::cerr;
 using std::endl;
+using std::make_shared;
 
 //{{{ SSHTransfer -------------------------------------------------------------
 
@@ -98,10 +101,11 @@ void SSHTransfer::perform(DataProvider *dataprovider,
     Debug::debug()->dbg("Remote command: %s", remote.c_str());
 
     SubProcess p;
-    p.setPipeDirection(STDIN_FILENO, SubProcess::ParentToChild);
+    auto pipe = make_shared<ParentToChildPipe>();
+    p.setChildFD(STDIN_FILENO, pipe);
     p.spawn("ssh", makeArgs(remote));
 
-    int fd = p.getPipeFD(STDIN_FILENO);
+    int fd = pipe->parentFD();
     try {
         dataprovider->prepare();
         prepared = true;
@@ -125,12 +129,10 @@ void SSHTransfer::perform(DataProvider *dataprovider,
 	    }
         }
     } catch (...) {
-	close(fd);
         if (prepared)
             dataprovider->finish();
         throw;
     }
-    close(fd);
 
     dataprovider->finish();
     int status = p.wait();
@@ -366,12 +368,13 @@ SFTPTransfer::SFTPTransfer(const RootDirURLVector &urlv)
     Debug::debug()->trace("SFTPTransfer::SFTPTransfer(%s)",
 			  parser.getURL().c_str());
 
-    m_process.setPipeDirection(STDIN_FILENO, SubProcess::ParentToChild);
-    m_process.setPipeDirection(STDOUT_FILENO, SubProcess::ChildToParent);
-    m_process.spawn("ssh", makeArgs());
+    m_req = make_shared<ParentToChildPipe>();
+    m_process.setChildFD(STDIN_FILENO, m_req);
 
-    m_fdreq = m_process.getPipeFD(STDIN_FILENO);
-    m_fdresp = m_process.getPipeFD(STDOUT_FILENO);
+    m_resp = make_shared<ChildToParentPipe>();
+    m_process.setChildFD(STDOUT_FILENO, m_resp);
+
+    m_process.spawn("ssh", makeArgs());
 
     SFTPPacket initpkt;
     initpkt.addByte(SSH_FXP_INIT);
@@ -394,8 +397,8 @@ SFTPTransfer::~SFTPTransfer()
     Debug::debug()->trace("SFTPTransfer::~SFTPTransfer()");
 
     if (m_process.getChildPID() != -1) {
-	close(m_fdreq);
-	close(m_fdresp);
+        m_req->close();
+	m_resp->close();
 
 	int status = m_process.wait();
 	if (status != 0)
@@ -640,7 +643,7 @@ void SFTPTransfer::sendPacket(SFTPPacket &pkt)
     size_t buflen = bv.size();
 
     while (buflen) {
-	ssize_t len = write(m_fdreq, bufp, buflen);
+	ssize_t len = write(m_req->parentFD(), bufp, buflen);
 	if (len < 0)
 	    throw KSystemError("SFTPTransfer::sendPacket: write failed",
 			       errno);
@@ -653,7 +656,7 @@ void SFTPTransfer::sendPacket(SFTPPacket &pkt)
 void SFTPTransfer::recvBuffer(unsigned char *bufp, size_t buflen)
 {
     while (buflen) {
-	ssize_t len = read(m_fdresp, bufp, buflen);
+	ssize_t len = read(m_resp->parentFD(), bufp, buflen);
 	if (len < 0)
 	    throw KSystemError("SFTPTransfer::recvPacket: read failed",
 			       errno);

kdumptool/process.cc
@@ -341,7 +341,7 @@ class Input : public ProcessFilter::IO {
 // -----------------------------------------------------------------------------
 void Input::setupSubProcess(SubProcess &p)
 {
-    p.setPipeDirection(m_fd, SubProcess::ParentToChild);
+    p.setChildFD(m_fd, make_shared<ParentToChildPipe>());
 }
 
 //}}}
@@ -388,7 +388,7 @@ void IStream::handleEvents(MultiplexIO &io, SubProcess &p)
 	bufptr += cnt;
 
 	if (m_input->eof()) {
-	    p.setPipeDirection(m_fd, SubProcess::None);
+	    p.setChildFD(m_fd, nullptr);
 	    io.deactivate(pollidx);
 	}
     }
@@ -409,7 +409,7 @@ class Output : public ProcessFilter::IO {
 // -----------------------------------------------------------------------------
 void Output::setupSubProcess(SubProcess &p)
 {
-    p.setPipeDirection(m_fd, SubProcess::ChildToParent);
+    p.setChildFD(m_fd, make_shared<ChildToParentPipe>());
 }
 
 //}}}
@@ -451,7 +451,7 @@ void OStream::handleEvents(MultiplexIO &io, SubProcess &p)
 	    cnt = 0;
 
 	if (!cnt) {
-	    p.setPipeDirection(m_fd, SubProcess::None);
+	    p.setChildFD(m_fd, nullptr);
 	    io.deactivate(pollidx);
 	}
 	m_output->write(buf, cnt);

kdumptool/sshtransfer.h
@@ -19,12 +19,12 @@
 #ifndef SSHTRANSFER_H
 #define SSHTRANSFER_H
 
+#include <memory>
+
 #include "global.h"
 #include "stringutil.h"
-#include "fileutil.h"
 #include "rootdirurl.h"
 #include "process.h"
-#include "socket.h"
 #include "transfer.h"
 
 //{{{ SSHTransfer --------------------------------------------------------------
@@ -228,7 +228,7 @@ class SFTPTransfer : public URLTransfer {
 
     private:
 	SubProcess m_process;
-	int m_fdreq, m_fdresp;
+        std::shared_ptr<SubProcessPipe> m_req, m_resp;
 	unsigned long m_proto_ver; // remote SFTP protocol version
 	unsigned long m_lastid;
 

kdumptool/testprocess.cc
@@ -30,6 +30,7 @@
 using std::cout;
 using std::cerr;
 using std::endl;
+using std::shared_ptr;
 using std::make_shared;
 
 // -----------------------------------------------------------------------------
@@ -45,45 +46,40 @@ int main(int argc, char *argv[])
 
     Debug::debug()->setStderrLevel(Debug::DL_TRACE);
     try {
-	try {
-	    // This must throw an out_of_range exception
-	    cout << "Pipe 0 fd: " << p.getPipeFD(0) << endl;
-	    ++errors;
-	} catch(std::out_of_range e) {
-	    cout << "OK, pipe 0 not yet initialized" << endl;
-	}
+        shared_ptr<SubProcessFD> temp;
+        auto found = p.getChildFD(0);
+        if (found) {
+            cerr << "Uninitialized pipe is a "
+                 << typeid(*found).name() << endl;
+            ++errors;
+        } else
+            cout << "Uninitialized pipe is nullptr" << endl;
 
-	SubProcess::PipeDirection dir;
+        cout << "Checking ParentToChildPipe" << endl;
+        temp.reset(new ParentToChildPipe());
+	p.setChildFD(0, temp);
+        found = p.getChildFD(0);
+        if (typeid(*found) != typeid(ParentToChildPipe)) {
+            cerr << "Unexpected type " << typeid(*found).name() << endl;
+            ++errors;
+        }
 
-	dir = p.getPipeDirection(0);
-	cout << "Pipe 0 direction: " << dir << endl;
+        cout << "Checking ChildToParentPipe" << endl;
+        temp.reset(new ChildToParentPipe());
+	p.setChildFD(0, temp);
+        found = p.getChildFD(0);
+        if (typeid(*found) != typeid(ChildToParentPipe)) {
+            cerr << "Unexpected type " << typeid(*found).name() << endl;
+            ++errors;
+        }
 
-	p.setPipeDirection(0, SubProcess::ParentToChild);
-	dir = p.getPipeDirection(0);
-	cout << "Pipe 0 fd: " << p.getPipeFD(0)
-	     << ", direction: " << dir << endl;
-	if (dir != SubProcess::ParentToChild) {
-	    cerr << "  but expected " << SubProcess::ParentToChild << endl;
-	    ++errors;
-	}
-
-	p.setPipeDirection(0, SubProcess::ChildToParent);
-	dir = p.getPipeDirection(0);
-	cout << "Pipe 0 fd: " << p.getPipeFD(0)
-	     << ", direction: " << dir << endl;
-	if (dir != SubProcess::ChildToParent) {
-	    cerr << "  but expected " << SubProcess::ChildToParent << endl;
-	    ++errors;
-	}
-
-	p.setPipeDirection(0, SubProcess::None);
-	try {
-	    // This must throw an out_of_range exception
-	    cout << "Pipe 0 fd: " << p.getPipeFD(0) << endl;
-	    ++errors;
-	} catch(const std::out_of_range &e) {
-	    cout << "OK, pipe 0 de-initialized again" << endl;
-	}
+        cout << "Checking no pipe" << endl;
+	p.setChildFD(0, nullptr);
+        found = p.getChildFD(0);
+        if (found) {
+            cerr << "Pipe is still a " << typeid(*found).name() << endl;
+            ++errors;
+        }
 
 	StringVector v;
 	p.spawn("true", v);

commit a333c940eea08d5369c5ed136a25cface0a876a8

Explicitly close pipe to ssh on EOF from data provider
The write end of the pipe must be closed before calling p.wait(),
or the ssh process will continue waiting for more data forever.

Fixes: 7ac0bd39

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

kdumptool/sshtransfer.cc
@@ -133,6 +133,7 @@ void SSHTransfer::perform(DataProvider *dataprovider,
             dataprovider->finish();
         throw;
     }
+    pipe->close();
 
     dataprovider->finish();
     int status = p.wait();