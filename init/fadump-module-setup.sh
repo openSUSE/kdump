98ef25442903c2d50190f40cff28025b9c246d49
Petr Tesarik <ptesarik@suse.com>
2021-12-08 13:05:38 +0100

    Create fadump initrd from the fadumpinit dracut module
    The current mechanism works well if mkdumprd is used to build the
    initrd. Unfortunately, there are other ways that dracut is invoked
    automatically, e.g. from a post-install script when installing an
    updated kernel package. If that happens, firmware-assisted dump
    fails until the next mkdumprd invocation, because this new kernel
    becomes the default boot option, but the initrd does not contain
    fadump-specific bits.
    
    So, let's do things differently.
    
    The fadumpinit dracut module gets automatically included if
    KDUMP_FADUMP is enabled in kdump configuration. Note that it can
    still be explicitly omitted with a dracut command-line parameter.
    The content of the fadump-specific initrd is then generated by the
    install() function of this module, i.e. a second dracut instance is
    executed recursively from a dracut module.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- init/fadump-module-setup.sh
+++ init/fadump-module-setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+. /lib/kdump/setup-kdump.functions
+
 check() {
+    if kdump_get_config && test "$KDUMP_FADUMP" = "yes"; then
+        return 0
+    fi
+
     return 255
 }
 
@@ -9,6 +15,28 @@ depends() {
 }
 
 install() {
+    kdump_get_config || return 1
+
+    local _fadumpdir="$initdir/fadumproot"
+
+    mkdir -p "$_fadumpdir" || return 1
+
+    dinfo "****** Generating FADUMP initrd *******"
+    local _initrd="$_fadumpdir/initrd"
+    local -a _dracut_args=(
+        "--force"
+        "--hostonly"
+        "--add" "kdump"
+        "--omit" "plymouth resume usrmount zz-fadumpinit"
+        "--no-compress"
+        "--no-early-microcode"
+    )
+    local _dracut="${dracut_cmd:-dracut}"
+    "$_dracut" "${_dracut_args[@]}" "$_initrd" "$kernel" || return 1
+    kdump_unpack_initrd "$_initrd" "$_fadumpdir" || return 1
+    rm -f "$_initrd"
+    dinfo "****** Generating FADUMP initrd done *******"
+
     mv -f "$initdir/init" "$initdir/init.dracut"
     inst_binary "$moddir/init-fadump" /init
     chmod a+x "$initdir/init"
