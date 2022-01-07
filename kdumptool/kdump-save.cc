6ecd649422a6d75a1002326edfee14fc689e1010
Petr Tesarik <ptesarik@suse.com>
2022-01-07 18:06:15 +0100

    kdump-save: Remove rwFixup()
    This must be legacy code from the pre-dracut era. As of version
    055, dracut does not create _any_ fstab entries. If there is any
    fstab in the initrd, it must have been created by the kdump module,
    and that's certainly not a read-only mount.
    
    Signed-off-by: Petr Tesarik <ptesarik@suse.com>

--- kdumptool/kdump-save.cc
+++ kdumptool/kdump-save.cc
@@ -315,33 +315,6 @@ int EnvironmentOverride::set(const char *value)
         : unsetenv(m_name.c_str());
 }
 
-// handle remounting existing readonly mounts readwrite
-// mount -a works only for not yet mounted filesystems
-void rwFixup()
-{
-    FstabMountTable tbl;
-    MountTable::iterator it(tbl, MNT_ITER_FORWARD);
-    while (++it) {
-        if (it->hasOption("bind") ||
-            it->hasOption("rbind")) {
-            FilePath mpt(it->target());
-            if (string(it->fstype()) == "none" && !mpt.isWritable()) {
-                // remounting bind mounts needs special incantation
-                StringVector args { "none", mpt, "-o", "remount,rw" };
-                ProcessFilter().execute("mount", args);
-            }
-        } else if (it->hasOption("ro")) {
-                continue;
-        } else {
-            FilePath mpt(it->target());
-            if (!mpt.isWritable()) {
-                StringVector args { mpt, "-o", "remount,rw" };
-                ProcessFilter().execute("mount", args);
-            }
-        }
-    }
-}
-
 static void deleteDumps()
 {
     try {
@@ -400,8 +373,6 @@ static void execute()
         if (code)
             cerr << "Transfer exit code is " << code << endl;
     } else {
-        rwFixup();
-
         // pre-script
         const string &prescript = config->KDUMP_PRESCRIPT.value();
         if (!prescript.empty()) {
