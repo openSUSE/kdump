openSUSE/kdump

commit c01f0ef3038a3b1e2a6d88c919775da433e9a516

calibrate: Abuse crashkernel= to protect the ELF core header
Unfortunately, memmap only works on ACPI systems, so it cannot be
used e.g. for IBM POWER...

There is a platform-independent option "reserve=", but that cannot
be used either, because it disallows reading the elfcorehdr itself
from that region.

It seems like a good idea to abuse the crashkernel= option. First,
it must be recognized on all platforms that support crash dumps and
second, the region is still regarded as normal RAM, just never
allocated to the running kernel, except when loading a crash
kernel, but we know that the test VM will not do that.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

calibrate/run-qemu.py
@@ -99,7 +99,7 @@ def run_qemu(bindir, params, initrd, elfcorehdr):
     kernel_args = (
         'panic=1',
         'console=ttyS0',
-        'elfcorehdr=0x{0:x} memmap={1:d}K$0x{0:x}'.format(
+        'elfcorehdr=0x{0:x} crashkernel={1:d}K@0x{0:x}'.format(
             elfcorehdr.address, elfcorehdr.size),
         'root=kdump',
         'rootflags=bind',

commit 8cc0a84ede95af3c2c27d31884334415c9f51f9b

calibrate: Disable kASLR in the VM
The reservation with crashkernel= may fail, because the memory
region is already used by the kernel. Make memory layout more
predictable by disabling kASLR.

Fixes: c01f0ef303

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

calibrate/run-qemu.py
@@ -117,6 +117,7 @@ def run_qemu(bindir, params, initrd, elfcorehdr):
         extra_args = ()
     kernel_args = (
         'panic=1',
+        'nokaslr',
         'console={}'.format(console),
         'elfcorehdr=0x{0:x} crashkernel={1:d}K@0x{0:x}'.format(
             elfcorehdr.address, elfcorehdr.size),