openSUSE/kdump

commit b152a6d57cb387eb2d072cc715784c1d80c03307

calibrate/trackrss: Handle SIGTERM
Let the tracker survive a SIGTERM that is sent to all processes on
shutdown. I am not setting the handler to SIG_IGN, because systemd
would wait for 90 seconds before sending a SIGKILL. Instead,
terminate the process after 5 seconds.

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

calibrate/trackrss.c
@@ -23,6 +23,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <signal.h>
 #include <time.h>
 #include <pthread.h>
 #include <sys/types.h>
@@ -34,6 +35,9 @@
 #include <linux/netlink.h>
 #include <linux/genetlink.h>
 #include <linux/taskstats.h>
+
+/* How long to wait upon receipt of SIGTERM */
+#define SIGTERM_WAIT    5
 
 /* There seems to be no canonical constant for this... */
 #define CTRL_GENL_VERSION	0x2
@@ -851,6 +855,12 @@ static int start_systemd(char *argv[])
 	return 0;
 }
 
+static void sigterm_handler(int signo)
+{
+        sleep(SIGTERM_WAIT);
+        exit(0);
+}
+
 int main(int argc, char *argv[])
 {
 	struct connection conn;
@@ -858,6 +868,8 @@ int main(int argc, char *argv[])
 	char *meminfo[MAX_MEMINFO_LINES];
 	int infonum;
 	int ret;
+
+        signal(SIGTERM, sigterm_handler);
 
 	ret = init_mounts();
 	if (ret)

commit 8edbe16043f699999a1f26978b313a8eab5732e1

calibrate/trackrss: Continue processing taskstat after SIGTERM
Processes that terminate after trackrss receives SIGTERM are not
included in the log, which causes a major instability in the
reported values.

Fixes: b152a6d57c

Signed-off-by: Petr Tesarik <ptesarik@suse.com>

calibrate/trackrss.c
@@ -262,6 +262,7 @@ static int conn_start_recv(struct connection *conn)
 
 	sigemptyset(&sigs);
 	sigaddset(&sigs, SIGTERM);
+	sigaddset(&sigs, SIGALRM);
 	pthread_sigmask(SIG_BLOCK, &sigs, NULL);
 
 	err = pthread_create(&conn->recv_thread, NULL, recv_thread_fn, conn);
@@ -899,8 +900,12 @@ static int start_systemd(char *argv[])
 
 static void sigterm_handler(int signo)
 {
-        sleep(SIGTERM_WAIT);
-        exit(0);
+	alarm(SIGTERM_WAIT);
+}
+
+static void sigalrm_handler(int signo)
+{
+	exit(0);
 }
 
 int main(int argc, char *argv[])
@@ -912,6 +917,7 @@ int main(int argc, char *argv[])
 	int ret;
 
         signal(SIGTERM, sigterm_handler);
+	signal(SIGALRM, sigalrm_handler);
 
         ret = open_console(argv);
         if (ret)