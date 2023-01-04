openSUSE/kdump

commit ddfc483c17426b56686d048f2ab8b37495ef6ac0

print the qemu messages and trackrss log during calibrate
When compiled with the calibrate option, debugging problems
inside the executed qemu is very difficult as no output is printed.
Fix this by running a tail -f on both TRACKRSS_LOG and MESSAGES_LOG.

Also disable parallel build in spec file by runing make direcly and not through
the %cmake_build macro, as this turns out the make output synchronization feature
and if qemu hangs, none of its output is printed during build.

calibrate/run-qemu.py
@@ -258,6 +258,19 @@ def run_qemu(bindir, params, initrd, elfcorehdr):
             elfcorehdr.path, elfcorehdr.address),
         *extra_qemu_args,
     )
+
+    # create the log files and monitor them with tail -f 
+    # (redirected to stderr)
+    # for debugging possible problems inside the VM
+    f = open(params['MESSAGES_LOG'], "w")
+    f.close()
+    subprocess.Popen(["tail", "-f", params['MESSAGES_LOG']], stdout=2)
+    
+    f = open(params['TRACKRSS_LOG'], "w")
+    f.close()
+    subprocess.Popen(["tail", "-f", params['TRACKRSS_LOG']], stdout=2)
+
+    
     subprocess.call(qemu_args)
 
     results = dict()

packaging/suse/kdump.spec
@@ -145,7 +145,11 @@ export CXXFLAGS="%{optflags} -std=c++11"
 	-DCALIBRATE=OFF
 %endif
 
-%cmake_build
+# run make directly instead of cmake_build, which would run make in parallel
+# and try to group output, preventing any debugging output from qemu if it
+# fails to exit
+make VERBOSE=1
+
 
 %check
 %ctest

commit 7b197291e66e5bebdc5a32466fa6c772ea90271e

calibrate: properly kill the tail process
Fixes: ddfc483c17426b56686d048f2ab8b37495ef6ac0

calibrate/run-qemu.py
@@ -264,14 +264,17 @@ def run_qemu(bindir, params, initrd, elfcorehdr):
     # for debugging possible problems inside the VM
     f = open(params['MESSAGES_LOG'], "w")
     f.close()
-    subprocess.Popen(["tail", "-f", params['MESSAGES_LOG']], stdout=2)
+    tail_messages = subprocess.Popen(["tail", "-f", params['MESSAGES_LOG']], stdout=2)
     
     f = open(params['TRACKRSS_LOG'], "w")
     f.close()
-    subprocess.Popen(["tail", "-f", params['TRACKRSS_LOG']], stdout=2)
+    tail_trackrss = subprocess.Popen(["tail", "-f", params['TRACKRSS_LOG']], stdout=2)
 
     
     subprocess.call(qemu_args)
+
+    tail_messages.kill()
+    tail_trackrss.kill()
 
     results = dict()
 