openSUSE/kdump

commit 6070d3292bfe176e05e5e7b89c42ff832220df12

calibrate: use static IP address
DHCP sometimes fails in the OBS

calibrate/run-qemu.py
@@ -224,7 +224,7 @@ def run_qemu(bindir, params, initrd, elfcorehdr):
         extra_kernel_args.extend((
             'ifname=kdump0:{}'.format(mac),
             'bootdev=kdump0',
-            'ip=kdump0:dhcp'
+            'ip=192.168.0.2::192.168.0.1:255.255.255.0::kdump0'
         ))
 
     # Other arch-specific arguments

commit dece85d2b1d0bbb3d0e53087a61f20ac65f84353

calibrate: fix dracut ip parameter for static IP
Fixes: 6070d3292bfe176e05e5e7b89c42ff832220df12

calibrate/run-qemu.py
@@ -224,7 +224,7 @@ def run_qemu(bindir, params, initrd, elfcorehdr):
         extra_kernel_args.extend((
             'ifname=kdump0:{}'.format(mac),
             'bootdev=kdump0',
-            'ip=192.168.0.2::192.168.0.1:255.255.255.0::kdump0'
+            'ip=192.168.0.2::192.168.0.1:255.255.255.0::kdump0:none'
         ))
 
     # Other arch-specific arguments