openSUSE/kdump

commit 51a68c31497d1895602d5c6851e23a9027d9675d

Don't split by default
Currently, we are default'ing to "SPILT" as well as multi-threading
when KDUMPTOOL_FLAGS="". Ensure split is enabled only when it is set
explicitly with KDUMPTOOL_FLAGS="SPLIT".

Signed-off-by: Ankit Kumar <ankit@linux.vnet.ibm.com>

kdumptool/savedump.cc
@@ -294,15 +294,12 @@ void SaveDump::saveDump(const RootDirURLVector &urlv)
         if (cpus > online_cpus)
             cpus = online_cpus;
     }
-    if (!config->kdumptoolContainsFlag("NOSPLIT") &&
-        !config->kdumptoolContainsFlag("SINGLE") &&
+    if (!config->kdumptoolContainsFlag("SINGLE") &&
         cpus > 1) {
-	if (!useElf)
-	    m_split = cpus;
-	else
-	    cerr << "Splitting ELF dumps is not supported." << endl;
 
-        if (config->kdumptoolContainsFlag("SPLIT")) {
+        /* The check for NOSPLIT is for backward compatibility */
+        if (config->kdumptoolContainsFlag("SPLIT") &&
+            !config->kdumptoolContainsFlag("NOSPLIT")) {
             if (!useElf)
                 m_split = cpus;
             else

commit b7599b13aaf751632cf337eafd4f02d507215dbb

Fix NOSPLIT
Commit 51a68c31497d1895602d5c6851e23a9027d9675d broke the
NOSPLIT flag. Make it work as documented.

kdumptool/savedump.cc
@@ -271,12 +271,12 @@ void SaveDump::saveDump(const RootDirURLVector &urlv)
         if (cpus > online_cpus)
             cpus = online_cpus;
     }
+    /* The check for NOSPLIT is for backward compatibility */
     if (!config->kdumptoolContainsFlag("SINGLE") &&
+        !config->kdumptoolContainsFlag("NOSPLIT") &&
         cpus > 1) {
 
-        /* The check for NOSPLIT is for backward compatibility */
-        if (config->kdumptoolContainsFlag("SPLIT") &&
-            !config->kdumptoolContainsFlag("NOSPLIT")) {
+        if (config->kdumptoolContainsFlag("SPLIT")) {
             if (!useElf)
                 m_split = cpus;
             else