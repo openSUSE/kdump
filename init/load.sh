openSUSE/kdump

commit c01eb10ccf123d76bd1bd71d48ccdbbc6160dc8c

SELinux: temporary hack for bsc#1213721
init.sh is not allowed to dereference the /var/lib/kdump/kernel
symlink, while /bin/test is.

Later kexec also needs to dereference that symlink, but that was easy
to fix in the SELinux policy, while properly fixing the
test problem seems like a big task.

init/load.sh
@@ -370,8 +370,15 @@ fi
 # check if initrd and the kernel it was built for exist
 # return 6 if not, which is treated as success by 
 # the kdump-early service
-[[ -f "${kdump_initrd}" ]] || exit 6
-[[ -f "${kdump_kernel}" ]] || exit 6
+
+# FIXME: This is a fragile SELinux workaround: use /bin/test instead of [[ ]] 
+# because it runs with a different label which by coincidence
+# is allowed to dereference the symlink
+# see bsc#1213721
+#[[ -f "${kdump_initrd}" ]] || exit 6
+#[[ -f "${kdump_kernel}" ]] || exit 6
+/bin/test -f "${kdump_initrd}" || exit 6
+/bin/test -f "${kdump_kernel}" || exit 6
 
 if [ "$shrink" = yes ] ; then
     kdumptool calibrate --shrink > /dev/null

commit 07b17d8d45393fa0497a96d58cbd7bac615c65df

load.sh: fix the SELinux workaround for pre-usrmerge distros
on pre-usermerge distros /bin/test does not exist, it's
/usr/bin/test

Fixes: c01eb10ccf123d76bd1bd71d48ccdbbc6160dc8c

init/load.sh
@@ -313,8 +313,8 @@ fi
 # see bsc#1213721
 #[[ -f "${kdump_initrd}" ]] || exit 6
 #[[ -f "${kdump_kernel}" ]] || exit 6
-/bin/test -f "${kdump_initrd}" || exit 6
-/bin/test -f "${kdump_kernel}" || exit 6
+/usr/bin/test -f "${kdump_initrd}" || exit 6
+/usr/bin/test -f "${kdump_kernel}" || exit 6
 
 if [ "$shrink" = yes ] ; then
     kdumptool calibrate --shrink > /dev/null