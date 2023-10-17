openSUSE/kdump

commit 1f74c569032372b424c47301ae3a3a3a21e12cbf

Simplify kdump-save and initrd generation
This is the first of a series of changes aiming to substantially simplify
the kdump code base.

This is complete rewrite of kdump-save in bash achieving simplification by
deprecation of obsolete or complicated fatures and using existing dracut
functionality and external programs instead of our own re-implementaion in
kdump-save / kdumptool.

Following commits will clean up and remove most of the kdumptool code, most of
which is now unused.

Features deprecated:

- split dumps (saving to more than one targets at once)

  Originally this feature allowed to speed things up by multithreading and
  combining I/O bandwidth of more block devices.  The former is now obsoleted
  by multithreading support in makedumpfile.  Combining I/O badwidth seems too
  much of a corner case to keep things complicated.  Users with special needs
  like this can always write their own script to do exactly what they need and
  use it in KDUMP_TRANSFER

- KDUMPTOOL_FLAGS option removed

  Most of the flags dealt with the split dumps.
  XENALLDOMAINS is now default and can be disabled by including -X in
  MAKEDUMPFILE_OPTIONS

- notification e-mails

  Sending e-mail from the memory-restricted kdump environment is not ideal.
  The plan is to implement a simple notification service to be run after boot
  in the normal OS, checking for locally stored dumps. Network-stored dumps
  can be detected by the hosts that keep them.

- copying of the kernel image (KDUMP_COPY_KERNEL)

  The kernel image itself, as shipped in SUSE distributions, is not enough for
  debugging because it needs to be accompanied by the debug info. Determining
  which kernel to copy and mounting the respective file system is not easy and
  with the very limited usefulness is not worth the trouble.

These changes allow a huge simplification of everything pertaining to mounting
file systems. The only mounts needed are for the file, nfs and cifs targets.

dracut/module-setup.sh
@@ -2,19 +2,30 @@
 # module-setup.sh for kdump dracut module
 
 test -n "$KDUMP_LIBDIR" || KDUMP_LIBDIR=/usr/lib/kdump
-. "$KDUMP_LIBDIR"/setup-kdump.functions
 
-kdump_needed() {
+check() {
 	# Building a kdump initrd?
-	if [[ " $dracutmodules $add_dracutmodules $force_add_dracutmodules" == *\ $_mod\ * ]]; then
-		return 0
+	if ! [[ " $dracutmodules $add_dracutmodules $force_add_dracutmodules" == *\ $_mod\ * ]]; then
+		# Do not include kdump by default
+		return 1
 	fi
 
-	# Do not include kdump by default
-	return 1
-}
+	. "$KDUMP_LIBDIR"/kdump-read-config.sh || return 1
+	. "$KDUMP_LIBDIR"/setup-kdump.functions || return 1
 
-kdump_check_net() {
+	# no network needed by default
+	kdump_neednet=
+
+	# check if fence_kdump_send is installed and configured
+	if [ -f "$FENCE_KDUMP_SEND" ] &&
+	   [[ "$KDUMP_POSTSCRIPT" =~ "$FENCE_KDUMP_SEND" ]] ; then
+		# add fence_kdump_send to initrd automatically
+		KDUMP_REQUIRED_PROGRAMS="$KDUMP_REQUIRED_PROGRAMS $FENCE_KDUMP_SEND"
+		# set up network (unless explicitly disabled by KDUMP_NETCONFIG)
+		kdump_neednet=y
+	fi
+
+	# check if network is needed
 	if [ -z "$KDUMP_NETCONFIG" ]; then
 		# network explicitly disabled in configuration
 		kdump_neednet=
@@ -22,100 +33,9 @@ kdump_check_net() {
 		# always set up network
 		kdump_neednet=y
 	else
-		for protocol in "${kdump_Protocol[@]}" ; do
-		if [ "$protocol" != "file" -a "$protocol" != "srcfile" ]; then
-			kdump_neednet=y
-		fi
-		done
-
-		# network configuration
-		if [ -n "$KDUMP_SMTP_SERVER" -a -n "$KDUMP_NOTIFICATION_TO" ]; then
-		kdump_neednet=y
-		fi
+		# everything other than "file" needs network
+		[[ ${KDUMP_PROTO} == file ]] || kdump_neednet=y
 	fi
-}
-
-kdump_get_fs_type() {
-	local _dev="/dev/block/$1"
-	local _fstype
-	if [ -b "$_dev" ] && _fstype=$(get_fs_env "$_dev") ; then
-		host_fs_types["$(readlink -f "$_dev")"]="$_fstype"
-	elif _fstype=$(find_dev_fstype "$_dev"); then
-		host_fs_types["$_dev"]="$_fstype"
-	fi
-	return 1
-}
-
-kdump_add_host_dev() {
-	local _dev=$1
-	[[ " ${host_devs[@]} " == *" $_dev "* ]] && return
-	host_devs+=( "$_dev" )
-	check_block_and_slaves_all kdump_get_fs_type "$(get_maj_min "$_dev")"
-}
-
-kdump_add_mnt() {
-	local _idx=$1
-	local _dev="${kdump_dev[_idx]}"
-	local _mp="${kdump_mnt[_idx]}"
-
-	# Convert system root mounts to bind mounts
-	if [ "$KDUMP_FADUMP" = "yes" -a "${_mp%/*}" = "/kdump" ] ; then
-		mkdir -p "$initdir/etc"
-		echo "/sysroot $_mp none bind 0 0" >> "$initdir/etc/fstab"
-		return
-	fi
-
-	case "$_dev" in
-		UUID=*|LABEL=*|PARTUUID=*|PARTLABEL=*)
-			_dev=$(blkid -l -t "$_dev" -o device)
-			;;
-	esac
-	kdump_add_host_dev "$_dev"
-	host_fs_types["$_dev"]="${kdump_fstype[_idx]}"
-	if [ "${kdump_fstype[_idx]}" = btrfs ] ; then
-		for _dev in $() ; do
-			kdump_add_host_dev "$_dev"
-		done
-	fi
-
-	mkdir -p "$initdir/etc"
-	local _passno=2
-	[ "${kdump_fstype[_idx]}" = nfs ] && _passno=0
-	_dev=$(kdump_mount_dev "${kdump_dev[_idx]}")
-	echo "$_dev ${kdump_mnt[_idx]} ${kdump_fstype[_idx]} ${kdump_opts[_idx]} 0 $_passno" >> "$initdir/etc/fstab"
-}
-
-check() {
-	# Get configuration
-	kdump_get_config || return 1
-
-	kdump_needed || return 1
-
-	# no network needed by default
-	kdump_neednet=
-
-	# add mount points
-	if ! [[ $mount_needs ]] ; then
-		kdump_get_mountpoints || return 1
-
-		local _i=0
-		while [ $_i -lt ${#kdump_mnt[@]} ]
-		do
-		[ -n "${kdump_mnt[_i]}" ] && kdump_add_mnt $_i
-		_i=$((_i+1))
-		done
-	fi
-
-	# check if fence_kdump_send is installed and configured
-	if [ -f "$FENCE_KDUMP_SEND" ] &&
-		   [[ "$KDUMP_POSTSCRIPT" =~ "$FENCE_KDUMP_SEND" ]] ; then
-		# add fence_kdump_send to initrd automatically
-		KDUMP_REQUIRED_PROGRAMS="$KDUMP_REQUIRED_PROGRAMS $FENCE_KDUMP_SEND"
-		# set up network (unless explicitly disabled by KDUMP_NETCONFIG)
-		kdump_neednet=y
-	fi
-
-	kdump_check_net
 
 	return 0
 }
@@ -134,53 +54,14 @@ depends() {
 		_modules[$_mod]=
 	fi
 
-	for protocol in "${kdump_Protocol[@]}" ; do
-	if [ "$protocol" = "nfs" ]; then
-		_modules[nfs]=
-	fi
-	if [ "$protocol" = "cifs" -o "$protocol" = "smb" ]; then
-			_modules[cifs]=
-	fi
-	done
-
 	echo ${!_modules[@]}
-}
-
-kdump_add_mpath_dev() {
-	local _major=${1%:*}
-	local _minor=${1#*:}
-	local _wwid
-
-	eval _wwid=\$kdump_mpath_wwid_${_major}_${_minor}
-	if [ -n "$_wwid" ] ; then
-	kdump_mpath_wwids+=$(printf "%q " "wwid $_wwid")
-	fi
-}
-
-kdump_cmdline_zfcp() {
-	is_zfcp() {
-		local _dev=$1
-		local _devpath=$(cd -P /sys/dev/block/$_dev ; echo $PWD)
-		local _sdev _lun _wwpn _ccw
-
-		[ "${_devpath#*/sd}" == "$_devpath" ] && return 1
-		_sdev="${_devpath%%/block/*}"
-		[ -e ${_sdev}/fcp_lun ] || return 1
-		_ccw=$(cat ${_sdev}/hba_id)
-		_lun=$(cat ${_sdev}/fcp_lun)
-		_wwpn=$(cat ${_sdev}/wwpn)
-		echo "rd.zfcp=${_ccw},${_wwpn},${_lun}"
-	}
-	[[ $hostonly ]] || [[ $mount_needs ]] && {
-		for_each_host_dev_and_slaves_all is_zfcp
-	} | sort -u
 }
 
 kdump_ip_set_explicitly() {
 	local _opt
 	local opts
 
-	if [ "$KDUMP_FADUMP" = yes ]; then
+	if [ "$KDUMP_FADUMP" = true ]; then
 		_opts="`cat /proc/cmdline`"
 	else
 		_opts="$KDUMP_COMMANDLINE $KDUMP_COMMANDLINE_APPEND"
@@ -249,11 +130,6 @@ kdump_cmdline_ip() {
 	esac
 }
 
-cmdline_zfcp() {
-	local _arch=$(uname -m)
-	[ "$_arch" = "s390" -o "$_arch" = "s390x" ] && kdump_cmdline_zfcp
-}
-
 # Output variables:
 #	kdump_kmods  additional kernel modules updated
 #	kdump_ifmap  hardware network interface map updated
@@ -268,77 +144,111 @@ installkernel() {
 
 install() {
 	if [[ $hostonly_cmdline == "yes" ]] ; then
-		local _cmdline=$(cmdline_zfcp)
-		[ -n "$_cmdline" ] && printf "%s\n" "$_cmdline" >> "${initdir}/etc/cmdline.d/99kdump-zfcp.conf"
-
-	# don't call cmdline_net in a subshell, it sets global variables
-	_cmdline_f=$(mktemp) || return 1
-	cmdline_net > $_cmdline_f
-		_cmdline=$(< $_cmdline_f)
-	rm $_cmdline_f
+		# don't call cmdline_net in a subshell, it sets global variables
+		_cmdline_f=$(mktemp) || return 1
+		cmdline_net > $_cmdline_f
+		local _cmdline=$(< $_cmdline_f)
+		rm $_cmdline_f
 
 		[ -n "$_cmdline" ] && printf "%s\n" "$_cmdline" >> "${initdir}/etc/cmdline.d/99kdump-net.conf"
 	fi
 
-	# Get a list of required multipath devices
-	local kdump_mpath_wwids
-	kdump_map_mpath_wwid
-	for_each_host_dev_and_slaves_all kdump_add_mpath_dev
-
-	kdump_setup_files "$initdir" "$kdump_mpath_wwids"
+	# parse the configuration, check values and initialize defaults 
+	"$KDUMP_LIBDIR"/kdump-read-config.sh --print > ${initdir}/etc/kdump.conf
+	# remember the host name
+	hostname >> "${initdir}/etc/hostname.kdump"
+	# filter out problematic sysctl settings
+	kdump_filter_sysctl "${initdir}" #FIXME needed?
 
 	inst_hook cmdline 50 "$moddir/kdump-root.sh"
-	inst_hook cmdline 50 "$moddir/kdump-boot.sh"
+	inst_script "$moddir"/kdump-save /kdump/kdump-save
 	if dracut_module_included "systemd" ; then
-	inst_binary "$moddir/device-timeout-generator" \
-		"$systemdutildir"/system-generators/kdump-device-timeout-generator
+		inst_binary "$moddir/device-timeout-generator" \
+			"$systemdutildir"/system-generators/kdump-device-timeout-generator
 
-	inst_binary "$KDUMP_LIBDIR"/kdump-save /kdump/kdump-save
-	awk -v mountpoints="${kdump_mnt[*]}" '{
-		gsub(/@KDUMP_MOUNTPOINTS@/, mountpoints)
-		print
-		}' "$moddir/kdump-save.service.in" > \
-			"$initdir/$systemdsystemunitdir"/kdump-save.service
+		inst_simple "$moddir/kdump-save.service" "$systemdsystemunitdir/kdump-save.service"
 
-		local _d _mp
-		local _mnt
-		_d="$initdir/$systemdsystemunitdir"/initrd-switch-root.target.d
-		mkdir -p "$_d"
-		(
-			echo "[Unit]"
-			for _mp in "${kdump_mnt[@]}" ; do
-				_mnt=$(systemd-escape -p --suffix=mount "$_mp")
-				_d="$initdir/$systemdsystemunitdir/$_mnt".d
-				mkdir -p "$_d"
-				echo -e "[Unit]\nConditionPathExists=/proc/vmcore" \
-					 > "$_d"/kdump.conf
-				echo "Conflicts=$_mnt"
-			done
-		) > "$_d"/kdump.conf
-
-	mkdir -p "$initdir/$systemdsystemunitdir"/initrd.target.wants
-	ln_r "$systemdsystemunitdir"/kdump-save.service \
-		"$systemdsystemunitdir"/initrd.target.wants/kdump-save.service
+		mkdir -p "$initdir/$systemdsystemunitdir"/initrd.target.wants
+		ln_r "$systemdsystemunitdir"/kdump-save.service \
+			"$systemdsystemunitdir"/initrd.target.wants/kdump-save.service
 	else
-	[ "$KDUMP_FADUMP" != yes ] && \
 		inst_hook mount 30 "$moddir/mount-kdump.sh"
-
-	inst_hook pre-pivot 90 "$KDUMP_LIBDIR"/kdump-save
+		inst_hook pre-pivot 90 "$KDUMP_LIBDIR"/kdump-save
 	fi
+	
+	# per-protocol config
+	case ${KDUMP_PROTO} in
+		ssh)
+			inst_multiple ssh
+			[[ ${KDUMP_NET_TIMEOUT} -gt 0 ]] && inst_multiple ping
+			kdump_init_ssh
+			;;
+		ftp)
+			inst_multiple lftp /usr/lib64/lftp/*/proto-ftp.so
+			[[ ${KDUMP_NET_TIMEOUT} -gt 0 ]] && inst_multiple ping
+			;;
+		sftp)
+			inst_multiple lftp /usr/lib64/lftp/*/proto-sftp.so ssh
+			[[ ${KDUMP_NET_TIMEOUT} -gt 0 ]] && inst_multiple ping
+			kdump_init_ssh
+			;;
+		file)
+			[[ ${KDUMP_FREE_DISK_SIZE} -gt 0 ]] && inst_multiple df
+			
+			# dereference symlinks, because they might not work in the
+			# kdump environment when the directory is mounted elsewhere
+			KDUMP_SAVEDIR_REALPATH=$(realpath -m "${KDUMP_SAVEDIR#*://}")
+			echo "KDUMP_SAVEDIR='${KDUMP_SAVEDIR_REALPATH//\'/\'\\\'\'}'" >> ${initdir}/etc/kdump.conf
+			;;
+	esac
 
-	inst_multiple makedumpfile \
-	$KDUMP_REQUIRED_PROGRAMS
-	inst_simple $(type -p makedumpfile-R.pl) /kdump/makedumpfile-R.pl
-	chmod -x "$initdir/kdump/makedumpfile-R.pl"
-
-	# Install /etc/resolv.conf to provide initial DNS configuration. The file
-	# is resolved first to install directly the target file if it is a symlink.
-	# The real resolv.conf could be in a location that gets hidden by a mount
-	# done in the kdump environment, for instance, /etc/resolv.conf ->
-	# /run/netconfig/resolv.conf with tmpfs getting mounted on /run.
-	local resolv=$(realpath /etc/resolv.conf)
-	inst_simple "$resolv" /etc/resolv.conf
-
+	inst_multiple makedumpfile date sleep $KDUMP_REQUIRED_PROGRAMS
+	
+	if [ "$kdump_neednet" = y ]; then
+		# Install /etc/resolv.conf to provide initial DNS configuration. The file
+		# is resolved first to install directly the target file if it is a symlink.
+		local resolv=$(realpath /etc/resolv.conf)
+		inst_simple "$resolv" /etc/resolv.conf
+		inst_simple /etc/hosts
+		# set up hardware interfaces
+		kdump_setup_hwif "${initdir}"
+	fi
+	inst_simple /etc/hostname
 	inst_simple /usr/share/zoneinfo/UTC
 	inst_simple /etc/localtime
+}
+
+function kdump_init_ssh()						   # {{{
+{
+	local SSH_DIR="${initdir}/root/.ssh"
+	mkdir -m 0700 "${SSH_DIR}"
+
+	URL=${KDUMP_SAVEDIR#*://}
+	HOST="${URL%%/*}"
+	HOST="${HOST#*@}"
+
+	# host keys
+	if [[ "${KDUMP_HOST_KEY}" == "*" ]]; then
+		echo "StrictHostKeyChecking no" >> "${SSH_DIR}/config"
+	else 
+		echo "StrictHostKeyChecking yes" >> "${SSH_DIR}/config"
+		if [[ -z "${KDUMP_HOST_KEY}" ]]; then
+			ssh-keygen -F "${HOST}" > "${SSH_DIR}/known_hosts" || derror "SSH host key neither specified in KDUMP_HOST_KEY nor found by 'ssh-keygen -F $HOST'; SSH host key checking will fail."
+		else
+			echo "${HOST} ${KDUMP_HOST_KEY}" > "${SSH_DIR}/known_hosts"
+		fi
+	fi
+
+	# identity files
+	pushd ~root/.ssh >/dev/null #identity files are relative to this directory
+	[[ -z "${KDUMP_SSH_IDENTITY}" ]] && KDUMP_SSH_IDENTITY="id_rsa id_dsa id_ecdsa id_ed25519"
+	for i in ${KDUMP_SSH_IDENTITY}; do
+		[[ -f "$i" ]] || continue
+		cp "$i" "${SSH_DIR}/"
+		[[ -f "${i}.pub" ]] && cp "${i}.pub" "${SSH_DIR}/"
+		[[ -f "${i}-cert.pub" ]] && cp "${i}-cert.pub" "${SSH_DIR}/"
+		f=$(basename $i)
+		echo "IdentityFile /root/.ssh/${f}" >> "${SSH_DIR}/config"
+	done
+	popd >/dev/null
 }

.gitignore
@@ -25,7 +25,6 @@ calibrate/mkelfcorehdr
 calibrate/trackrss
 dracut/device-timeout-generator
 dracut/init-fadump
-dracut/kdump-save
 kdumptool/kdumptool
 kdumptool/testcanonical
 kdumptool/testconfig

CMakeLists.txt
@@ -172,6 +172,17 @@ ADD_CUSTOM_TARGET(
         sysconfig.kdump
 )
 
+INSTALL (
+    FILES
+        ${CMAKE_CURRENT_SOURCE_DIR}/kdump-read-config.sh
+    DESTINATION
+        /usr/lib/kdump
+    PERMISSIONS
+        OWNER_READ OWNER_WRITE OWNER_EXECUTE
+        GROUP_READ GROUP_EXECUTE
+        WORLD_READ WORLD_EXECUTE
+)
+
 INSTALL(FILES
     ${CMAKE_CURRENT_BINARY_DIR}/sysconfig.kdump
     DESTINATION

doc/man/kdump.7.txt.in
@@ -313,7 +313,7 @@ dump level   zero page  cache page cache private user data free page
 Automatic Reboot and Error handling
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 If you want to have your machine rebooted automatically after the dump has been
-copied, set _KDUMP_IMMEDIATE_REBOOT_ to _yes_. The variable
+copied, set _KDUMP_IMMEDIATE_REBOOT_ to _true_. The variable
 _KDUMP_CONTINUE_ON_ERROR_ controls if a shell should be opened if something goes
 wrong while saving the dump to be able to fixup manually. This is mostly for
 debugging. 

dracut/CMakeLists.txt
@@ -23,13 +23,6 @@ TARGET_INCLUDE_DIRECTORIES(device-timeout-generator
 TARGET_LINK_LIBRARIES(device-timeout-generator
     ${BLKID_LIBRARIES})
 
-ADD_EXECUTABLE(kdump-save
-    kdump-save.cc
-)
-TARGET_INCLUDE_DIRECTORIES(kdump-save
-    PUBLIC ${CMAKE_SOURCE_DIR}/kdumptool)
-TARGET_LINK_LIBRARIES(kdump-save common ${EXTRA_LIBS})
-
 INSTALL(
     TARGETS
         device-timeout-generator
@@ -38,10 +31,14 @@ INSTALL(
 )
 
 INSTALL (
-    TARGETS
-        kdump-save
+    FILES
+        ${CMAKE_CURRENT_SOURCE_DIR}/kdump-save
     DESTINATION
-        /usr/lib/kdump
+        /usr/lib/dracut/modules.d/99kdump
+    PERMISSIONS
+        OWNER_READ OWNER_WRITE OWNER_EXECUTE
+        GROUP_READ GROUP_EXECUTE
+        WORLD_READ WORLD_EXECUTE
 )
 
 INSTALL(
@@ -58,7 +55,6 @@ INSTALL(
 INSTALL(
     FILES
         ${CMAKE_CURRENT_SOURCE_DIR}/kdump-root.sh
-        ${CMAKE_CURRENT_SOURCE_DIR}/kdump-boot.sh
         ${CMAKE_CURRENT_SOURCE_DIR}/module-setup.sh
         ${CMAKE_CURRENT_SOURCE_DIR}/mount-kdump.sh
     DESTINATION
@@ -71,7 +67,7 @@ INSTALL(
 
 INSTALL(
     FILES
-        ${CMAKE_CURRENT_SOURCE_DIR}/kdump-save.service.in
+        ${CMAKE_CURRENT_SOURCE_DIR}/kdump-save.service
     DESTINATION
         /usr/lib/dracut/modules.d/99kdump
     PERMISSIONS

dracut/fadump-module-setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 test -n "$KDUMP_LIBDIR" || KDUMP_LIBDIR=/usr/lib/kdump
-. "$KDUMP_LIBDIR"/setup-kdump.functions
+. "$KDUMP_LIBDIR"/kdump-read-config.sh || return 1
+. "$KDUMP_LIBDIR"/setup-kdump.functions || return 1
 
 check() {
-    if kdump_get_config && test "$KDUMP_FADUMP" = "yes"; then
+    if test "$KDUMP_FADUMP" = "true"; then
         return 0
     fi
 
@@ -16,8 +17,6 @@ depends() {
 }
 
 install() {
-    kdump_get_config || return 1
-
     local _fadumpdir="$initdir/fadumproot"
 
     mkdir -p "$_fadumpdir" || return 1
@@ -32,6 +31,9 @@ install() {
         "--no-compress"
         "--no-early-microcode"
     )
+
+    [[ -n "${KDUMP_DRACUT_MOUNT_OPTION}" ]] && _dracut_args+=("--mount" "${KDUMP_DRACUT_MOUNT_OPTION}")
+
     local _dracut="${dracut_cmd:-dracut}"
     "$_dracut" "${_dracut_args[@]}" "$_initrd" "$kernel" || return 1
     kdump_unpack_initrd "$_initrd" "$_fadumpdir" || return 1

dracut/kdump-boot.sh
@@ -1,20 +1,0 @@
-#! /bin/sh
-# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
-# ex: ts=8 sw=4 sts=4 et filetype=sh
-
-# An Initrd with dump capturing support can boot a production kernel
-# as well (FADump). In such scenario, apply optimizations and enforce
-# bringing up dump target only while booting the capture kernel - this
-# is a kernel that
-#   a) has a /proc/vmcore file waiting to be saved.
-#   b) reboots after saving the dump.
-
-# apply multipath optimizations
-if [ -s /proc/vmcore ]; then
-    # Replace the multipath.conf file with the one optimized for kdump.
-    rm -f /etc/multipath.conf
-    mv /etc/multipath.conf.kdump /etc/multipath.conf
-else
-    # avoid enforing network bring up while booting production kernel.
-    rm -f /etc/cmdline.d/99kdump-net.conf
-fi

commit d063a38be024fd34cb2faad78e8ee65707c00aa9

s390: Explicitly request zFCP devices
[ reintroduces commit 81899709e4a12bea4819d4ccc10f960cb535b447
  which got removed by 1f74c569032372b424c47301ae3a3a3a21e12cbf
  although it is still needed ]

When using NPIV with LUN autoscan, dracut does not save the port and
LUN information. However, LUN autoscan is always disabled in kdump
environment, so the LUNs will never appear.

To fix this case, the full information must be saved explicitly in a
kdump initrd.

Fixes: 1f74c569032372b424c47301ae3a3a3a21e12cbf

dracut/module-setup.sh
@@ -136,12 +136,17 @@ installkernel() {
 }
 
 install() {
+	local _arch=$(uname -m)
+
 	# generate command line for network configuration
 	kdump_cmdline_ip > "${initdir}/etc/cmdline.d/99kdump-net.conf"
 	[[ -s "${initdir}/etc/cmdline.d/99kdump-net.conf" ]] || rm "${initdir}/etc/cmdline.d/99kdump-net.conf"
 
 	# prevent dracut emergency shell unless kdump-save would run a shell anyway
 	kdump_save_may_run_shell || echo "rd.emergency=reboot rd.shell=0" > "${initdir}/etc/cmdline.d/99no-emerg.conf"
+
+	# S390: Explicitly request zFCP devices 
+	[ "$_arch" = "s390" -o "$_arch" = "s390x" ] && kdump_cmdline_zfcp > "${initdir}/etc/cmdline.d/99kdump-zfcp.conf"
 
 	# parse the configuration, check values and initialize defaults 
 	"$KDUMP_LIBDIR"/kdump-read-config.sh --print > ${initdir}/etc/kdump.conf
@@ -264,3 +269,23 @@ function kdump_save_may_run_shell
 
 	return 1
 }
+
+function kdump_cmdline_zfcp() {
+	is_zfcp() {
+		local _dev=$1
+		local _devpath=$(cd -P /sys/dev/block/$_dev ; echo $PWD)
+		local _sdev _lun _wwpn _ccw
+
+		[ "${_devpath#*/sd}" == "$_devpath" ] && return 1
+		_sdev="${_devpath%%/block/*}"
+		[ -e ${_sdev}/fcp_lun ] || return 1
+		_ccw=$(cat ${_sdev}/hba_id)
+		_lun=$(cat ${_sdev}/fcp_lun)
+		_wwpn=$(cat ${_sdev}/wwpn)
+		echo "rd.zfcp=${_ccw},${_wwpn},${_lun}"
+	}
+	[[ $hostonly ]] || [[ $mount_needs ]] && {
+		for_each_host_dev_and_slaves_all is_zfcp
+	} | sort -u
+}
+