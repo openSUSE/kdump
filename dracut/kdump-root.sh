1a4e26f963fdb894ebb0fc451a1f2f6d462dba29
Jiri Bohac <jbohac@suse.cz>
2024-03-05 22:52:59 +0100

    clean up kdump-root.sh
    kdump-root.sh is a dracut cmdline hook that sets
    rootok=1 to prevent dracut from failing because dracut
    does not understand root=kdump
    
    There is no need to check if root=kdump - the kdump module is
    only included in kdump initrds so rootok=1 can be set
    unconditionally.

--- dracut/kdump-root.sh
+++ dracut/kdump-root.sh
@@ -1,11 +1,5 @@
-#! /bin/sh
-# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
-# ex: ts=8 sw=4 sts=4 et filetype=sh
-
-# This script is sourced, so root should be set. But let's be paranoid
-[ -z "$root" ] && root=$(getarg root=)
-
-# kdump initrd uses a special directory structure
-if [ "$root" = "kdump" ]; then
-    rootok=1
-fi
+#!/bin/sh
+# we don't mount root in kdump
+# prevent dracut from failing because it does not
+# understand root=kdump
+rootok=1
