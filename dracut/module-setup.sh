a29fe3c07d0c3979ff73c3d63699ae6026c996d4
Jiri Bohac <jbohac@suse.cz>
2024-03-28 17:37:01 +0100

    remove dracut parse-root.sh hook
    This supplements
    commit 6217d2fbd87bc7d8cf33f284bcf033ce962c780e ("mask the dracut-rootfs-generator")
    which masked the dracut-root-fs-generator, preventing dracut from
    waiting for root device in its initqueue.
    
    However, dracut commit 86c8a5a7c2573645e67537fb9975efab808d42c9
    ("fix(dracut-systemd): rootfs-generator cannot write outside of generator dir")
    moved part of the generator code to a new cmdline hook: 00-parse-root.sh.
    Now this hook causes the same problem originally fixed by masking the generator.
    
    Remove this hook as well.

--- dracut/module-setup.sh
+++ dracut/module-setup.sh
@@ -161,6 +161,7 @@ install() {
 	# prevent mounting, fscking and waiting for root 
 	mkdir -p "$initdir/etc/systemd/system-generators"
 	ln -s /dev/null "$initdir/etc/systemd/system-generators/dracut-rootfs-generator"
+	rm -f "$initdir/lib/dracut/hooks/cmdline/00-parse-root.sh"
 	
 	inst_script "$moddir"/kdump-save /kdump/kdump-save
 	inst_simple "$moddir/kdump-save.service" "$systemdsystemunitdir/kdump-save.service"
