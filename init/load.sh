openSUSE/kdump

commit 1f74c569032372b424c47301ae3a3a3a21e12cbf

Simplify kdump-save and initrd generation
This is the first of a series of changes aiming to substantially simplify
the kdump code base.

This is complete rewrite of kdump-save in bash achieving simplification by
deprecation of obsolete or complicated fatures and using existing dracut
functionality and external programs instead of our own re-implementaion in
kdump-save / kdumptool.

Following commits will clean up and remove most of the kdumptool code, most of
which is now unused.

Features deprecated:

- split dumps (saving to more than one targets at once)

  Originally this feature allowed to speed things up by multithreading and
  combining I/O bandwidth of more block devices.  The former is now obsoleted
  by multithreading support in makedumpfile.  Combining I/O badwidth seems too
  much of a corner case to keep things complicated.  Users with special needs
  like this can always write their own script to do exactly what they need and
  use it in KDUMP_TRANSFER

- KDUMPTOOL_FLAGS option removed

  Most of the flags dealt with the split dumps.
  XENALLDOMAINS is now default and can be disabled by including -X in
  MAKEDUMPFILE_OPTIONS

- notification e-mails

  Sending e-mail from the memory-restricted kdump environment is not ideal.
  The plan is to implement a simple notification service to be run after boot
  in the normal OS, checking for locally stored dumps. Network-stored dumps
  can be detected by the hosts that keep them.

- copying of the kernel image (KDUMP_COPY_KERNEL)

  The kernel image itself, as shipped in SUSE distributions, is not enough for
  debugging because it needs to be accompanied by the debug info. Determining
  which kernel to copy and mounting the respective file system is not easy and
  with the very limited usefulness is not worth the trouble.

These changes allow a huge simplification of everything pertaining to mounting
file systems. The only mounts needed are for the file, nfs and cifs targets.

init/load.sh
@@ -342,7 +342,7 @@ function rebuild_kdumprd()
 # MAIN PROGRAM STARTS HERE
 #
 
-eval $($KDUMPTOOL dump_config)
+. /usr/lib/kdump/kdump-read-config.sh
 
 if [ $((${KDUMP_VERBOSE:-0} & 4)) -gt 0 ] ; then
     function kdump_echo()
@@ -394,7 +394,7 @@ if [ "$shrink" = yes ] ; then
     $KDUMPTOOL calibrate --shrink > /dev/null
 fi
 
-if [ "$KDUMP_FADUMP" = "yes" ] ; then
+if [ "$KDUMP_FADUMP" = "true" ] ; then
     load_kdump_fadump
 else
     fadump_bootloader off

.gitignore
@@ -25,7 +25,6 @@ calibrate/mkelfcorehdr
 calibrate/trackrss
 dracut/device-timeout-generator
 dracut/init-fadump
-dracut/kdump-save
 kdumptool/kdumptool
 kdumptool/testcanonical
 kdumptool/testconfig

CMakeLists.txt
@@ -172,6 +172,17 @@ ADD_CUSTOM_TARGET(
         sysconfig.kdump
 )
 
+INSTALL (
+    FILES
+        ${CMAKE_CURRENT_SOURCE_DIR}/kdump-read-config.sh
+    DESTINATION
+        /usr/lib/kdump
+    PERMISSIONS
+        OWNER_READ OWNER_WRITE OWNER_EXECUTE
+        GROUP_READ GROUP_EXECUTE
+        WORLD_READ WORLD_EXECUTE
+)
+
 INSTALL(FILES
     ${CMAKE_CURRENT_BINARY_DIR}/sysconfig.kdump
     DESTINATION

doc/man/kdump.7.txt.in
@@ -313,7 +313,7 @@ dump level   zero page  cache page cache private user data free page
 Automatic Reboot and Error handling
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 If you want to have your machine rebooted automatically after the dump has been
-copied, set _KDUMP_IMMEDIATE_REBOOT_ to _yes_. The variable
+copied, set _KDUMP_IMMEDIATE_REBOOT_ to _true_. The variable
 _KDUMP_CONTINUE_ON_ERROR_ controls if a shell should be opened if something goes
 wrong while saving the dump to be able to fixup manually. This is mostly for
 debugging. 

dracut/CMakeLists.txt
@@ -23,13 +23,6 @@ TARGET_INCLUDE_DIRECTORIES(device-timeout-generator
 TARGET_LINK_LIBRARIES(device-timeout-generator
     ${BLKID_LIBRARIES})
 
-ADD_EXECUTABLE(kdump-save
-    kdump-save.cc
-)
-TARGET_INCLUDE_DIRECTORIES(kdump-save
-    PUBLIC ${CMAKE_SOURCE_DIR}/kdumptool)
-TARGET_LINK_LIBRARIES(kdump-save common ${EXTRA_LIBS})
-
 INSTALL(
     TARGETS
         device-timeout-generator
@@ -38,10 +31,14 @@ INSTALL(
 )
 
 INSTALL (
-    TARGETS
-        kdump-save
+    FILES
+        ${CMAKE_CURRENT_SOURCE_DIR}/kdump-save
     DESTINATION
-        /usr/lib/kdump
+        /usr/lib/dracut/modules.d/99kdump
+    PERMISSIONS
+        OWNER_READ OWNER_WRITE OWNER_EXECUTE
+        GROUP_READ GROUP_EXECUTE
+        WORLD_READ WORLD_EXECUTE
 )
 
 INSTALL(
@@ -58,7 +55,6 @@ INSTALL(
 INSTALL(
     FILES
         ${CMAKE_CURRENT_SOURCE_DIR}/kdump-root.sh
-        ${CMAKE_CURRENT_SOURCE_DIR}/kdump-boot.sh
         ${CMAKE_CURRENT_SOURCE_DIR}/module-setup.sh
         ${CMAKE_CURRENT_SOURCE_DIR}/mount-kdump.sh
     DESTINATION
@@ -71,7 +67,7 @@ INSTALL(
 
 INSTALL(
     FILES
-        ${CMAKE_CURRENT_SOURCE_DIR}/kdump-save.service.in
+        ${CMAKE_CURRENT_SOURCE_DIR}/kdump-save.service
     DESTINATION
         /usr/lib/dracut/modules.d/99kdump
     PERMISSIONS

dracut/fadump-module-setup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 test -n "$KDUMP_LIBDIR" || KDUMP_LIBDIR=/usr/lib/kdump
-. "$KDUMP_LIBDIR"/setup-kdump.functions
+. "$KDUMP_LIBDIR"/kdump-read-config.sh || return 1
+. "$KDUMP_LIBDIR"/setup-kdump.functions || return 1
 
 check() {
-    if kdump_get_config && test "$KDUMP_FADUMP" = "yes"; then
+    if test "$KDUMP_FADUMP" = "true"; then
         return 0
     fi
 
@@ -16,8 +17,6 @@ depends() {
 }
 
 install() {
-    kdump_get_config || return 1
-
     local _fadumpdir="$initdir/fadumproot"
 
     mkdir -p "$_fadumpdir" || return 1
@@ -32,6 +31,9 @@ install() {
         "--no-compress"
         "--no-early-microcode"
     )
+
+    [[ -n "${KDUMP_DRACUT_MOUNT_OPTION}" ]] && _dracut_args+=("--mount" "${KDUMP_DRACUT_MOUNT_OPTION}")
+
     local _dracut="${dracut_cmd:-dracut}"
     "$_dracut" "${_dracut_args[@]}" "$_initrd" "$kernel" || return 1
     kdump_unpack_initrd "$_initrd" "$_fadumpdir" || return 1

dracut/kdump-boot.sh
@@ -1,20 +1,0 @@
-#! /bin/sh
-# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
-# ex: ts=8 sw=4 sts=4 et filetype=sh
-
-# An Initrd with dump capturing support can boot a production kernel
-# as well (FADump). In such scenario, apply optimizations and enforce
-# bringing up dump target only while booting the capture kernel - this
-# is a kernel that
-#   a) has a /proc/vmcore file waiting to be saved.
-#   b) reboots after saving the dump.
-
-# apply multipath optimizations
-if [ -s /proc/vmcore ]; then
-    # Replace the multipath.conf file with the one optimized for kdump.
-    rm -f /etc/multipath.conf
-    mv /etc/multipath.conf.kdump /etc/multipath.conf
-else
-    # avoid enforing network bring up while booting production kernel.
-    rm -f /etc/cmdline.d/99kdump-net.conf
-fi

commit b6486e1f11099a63046ae0ee1d19384cbd72a64d

fix KDUMP_AUTO_RESIZE
Two problems were found with KDUMP_AUTO_RESIZE=true, both present since the
rewrite in v1.9

1) the load.sh script wrongly compared the value to "yes" instead of "true"
2) kdumptool calibrate did not handle exceptions in memory shrinking

Fix both problems.

Fixes: 1f74c56903

init/load.sh
@@ -313,7 +313,7 @@ if [ "$update" = yes ] ; then
     rebuild_kdumprd || exit 1
 fi
 
-if [ "$shrink" = yes ] ; then
+if [ "$shrink" = true ] ; then
     kdumptool calibrate --shrink > /dev/null
 fi
 

kdumptool/calibrate.cc
@@ -1150,6 +1150,12 @@ int main(int argc, char* argv[])
 #endif
 
     if (m_shrink)
-        shrink_crash_size(required << 10);
+	try {
+		shrink_crash_size(required << 10);
+	}
+	catch(std::runtime_error &e) {
+		cerr << "Error shrinking reserved memory: " << e.what() << endl;
+		exit(1);
+	}
 }
 