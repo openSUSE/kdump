openSUSE/kdump

commit 75834c3f692f26139e12a08e162569e6e211ed11

[RFC] sysconfig/kdump: change KDUMP_CPU default value to 0 (#45)
By default, use all available CPUs to collect the dump instead of just
one CPU. This reduces the dump collection time significantly, specially
for the systems with very large memory configuration.

The graph below show the time reduction in dump collection on large
memory system from 4 Hours to 10 Minutes.

                               |
                       14000   | *
                               |
                               |
                               |
                               |
                               |
                               |
                        1600   |      *
                               |
                               |
                        1400   |
 execution Time (Sec)          |
                               |
                        1200   |
                               |
                               |
                        1000   |
                               |            *
                               |
                        800    |
                               |
                               |                   *
                        600    |
                               |                          *     *
                               |
                                ------------------------------------
                                   1    9    17    25     33    41

                                          Number of Threads

System details:
Architecture: PowerPC
/proc/vmcore size: 3.7T
Filter level: 1

Similar tests with different filter levels, 31 and 16, also show
significant reduction in time consumption for dump collection.

Although the above tests were performed only on the PowerPC
architecture, but I think using all CPUs for dump collection will have
performance benefits on other architectures too.

Signed-off-by: Sourabh Jain <sourabhjain@linux.ibm.com>

sysconfig.kdump.in
@@ -13,17 +13,15 @@
 KDUMP_KERNELVER=""
 
 ## Type:	integer
-## Default:	1
+## Default:	0
 ## ServiceRestart:	kdump
 #
-# Number of CPUs to be used in the kdump environment. You may want to
-# increase the number if computing power is the bottleneck in your setup.
-#
-# If the value is zero, use all available CPUs.
+# Number of CPUs to be used in the kdump environment. The default value is 0,
+# which means all available CPUs will be used to collect the dump."
 #
 # See also: kdump(5).
 #
-KDUMP_CPUS=1
+KDUMP_CPUS=0
 
 ## Type:	string
 ## Default:	""

commit 95378d3924a5b669f283bd4726c4bcb4c99b708f

Set default KDUMP_CPUS to 32
Commits 75834c3f692f26139e12a08e162569e6e211ed11 and
60525b4afa680650c9c85b4dc26372b34481d908 changed the default KDUMP_CPUS from
1 to 0 (meaning all CPUs). This was requested by IBM in jsc#PED-9894, bsc#1237754.

This change wa short-sighted: on machines with very large numbers of CPUs this
default does not scale. The crashkernel memory requrements grow beyond
reasonable. bsc#1240769 shows a system with 640 CPUs which does not work unless
1.8 GB memory is reserved.

Change the default to 32. This should require at most a couple tens of MBs extra
reserved memory compared to the original default of 1.

30-40 CPUs should also roughly be enough to fully load a PCIe device such as a
disk controller or a NIC used to save the dump.  A similar result can be seen
in the graph provided in the of commit 75834c3f692f26139e12a08e162569e6e211ed11
- the performance did not improve with more than ~30 threads.

Fixes: 75834c3f692f26139e12a08e162569e6e211ed11
Fixes: 60525b4afa680650c9c85b4dc26372b34481d908

doc/man/kdump.5.txt.in
@@ -79,7 +79,7 @@ If the value is zero, all available CPUs are used, i.e. the _nr_cpus_
 parameter is not added to the kdump kernel command line and _--num-threads_ 
 is set to the number of CPUs.
 
-Default is 0.
+Default is 32.
 
 
 KDUMP_COMMANDLINE

kdump-read-config.sh
@@ -10,7 +10,7 @@ function kdump_read_config_main()
 	option string 	 KDUMP_COMMANDLINE ""
 	option string 	 KDUMP_COMMANDLINE_APPEND ""
 	option bool 	 KDUMP_CONTINUE_ON_ERROR true
-	option int 	 KDUMP_CPUS 0
+	option int 	 KDUMP_CPUS 32
 	option string 	 KDUMP_DUMPFORMAT "compressed"
 	option int 	 KDUMP_DUMPLEVEL 31
 	option bool 	 KDUMP_FADUMP false

sysconfig.kdump.in
@@ -13,15 +13,17 @@
 KDUMP_KERNELVER=""
 
 ## Type:	integer
-## Default:	0
+## Default:	32
 ## ServiceRestart:	kdump
 #
-# Number of CPUs to be used in the kdump environment. The default value is 0,
-# which means all available CPUs will be used to collect the dump."
+# Number of CPUs to be used in the kdump environment. You may want to
+# increase the number if computing power is the bottleneck in your setup.
+#
+# If the value is zero, use all available CPUs.
 #
 # See also: kdump(5).
 #
-KDUMP_CPUS=0
+KDUMP_CPUS=32
 
 ## Type:	string
 ## Default:	""